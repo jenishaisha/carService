b516c7d0e7d1943ff5259ee45de2201de18cce07
purojinshamini <purojinshamini@gmail.com>
2025-05-13 07:12:03 +0530

    contact completed

--- carservice/src/app/contact/contact.component.ts
+++ carservice/src/app/contact/contact.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatListModule} from '@angular/material/list';
+
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatIconModule, MatListModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  typesOfServices: string[] = ['Oil Service', 'Denting & Painting', ' Interior & Exterior Cleaning', 'Engine and Transmission', ' Other'];
 
 }
